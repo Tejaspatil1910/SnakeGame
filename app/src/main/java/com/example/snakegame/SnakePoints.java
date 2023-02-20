201d097d2802989c6876bca6f43fc022e8f6c5eb
Tejas Patil <tejaspatil191001@gmail.com>
2023-02-20 23:42:07 +0530

    Snake Game

---
+++ app/src/main/java/com/example/snakegame/SnakePoints.java
@@ -0,0 +1,27 @@
+package com.example.snakegame;
+
+public class SnakePoints {
+    private int positionX, positionY;
+
+    public SnakePoints(int positionX, int positionY) {
+        this.positionX = positionX;
+        this.positionY = positionY;
+    }
+
+    public int getPositionX() {
+        return positionX;
+    }
+
+    public int getPositionY() {
+        return positionY;
+    }
+
+    public void setPositionX(int i) {
+    }
+
+    public void setPositionY() {
+    }
+
+    public void setPositionY(int i) {
+    }
+}
